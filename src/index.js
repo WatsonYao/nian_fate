9ade01b212b4587c0dbb2a667a30fa99514f1282
watsonyao <yaoyuan131617@gmail.com>
2019-09-29 13:19:21 +0800

    feat: add demo
    https://github.com/XMandarava/Demo-Electron-Notepad

--- src/index.js
+++ src/index.js
@@ -1,57 +1,109 @@
-import { app, BrowserWindow } from 'electron';
+import { ipcRenderer, remote } from 'electron';
+const { Menu, MenuItem, dialog } = remote;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+let currentFile = null; //当前文档保存的路径
+let isSaved = true;     //当前文档是否已保存
+let txtEditor = document.getElementById('txtEditor'); //获得TextArea文本框的引用
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+document.title = "Notepad - Untitled"; //设置文档标题，影响窗口标题栏名称
 
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+//给文本框增加右键菜单
+const contextMenuTemplate=[
+    { role: 'undo' },       //Undo菜单项
+    { role: 'redo' },       //Redo菜单项
+    { type: 'separator' },  //分隔线
+    { role: 'cut' },        //Cut菜单项
+    { role: 'copy' },       //Copy菜单项
+    { role: 'paste' },      //Paste菜单项
+    { role: 'delete' },     //Delete菜单项
+    { type: 'separator' },  //分隔线
+    { role: 'selectall' }   //Select All菜单项
+];
+const contextMenu=Menu.buildFromTemplate(contextMenuTemplate);
+txtEditor.addEventListener('contextmenu', (e)=>{
+    e.preventDefault();
+    contextMenu.popup(remote.getCurrentWindow());
+});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+//监控文本框内容是否改变
+txtEditor.oninput=(e)=>{
+    if(isSaved) document.title += " *";
+    isSaved=false;
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+//监听与主进程的通信
+ipcRenderer.on('action', (event, arg) => {
+    switch(arg){
+    case 'new': //新建文件
+        askSaveIfNeed();
+        currentFile=null;
+        txtEditor.value='';
+        document.title = "Notepad - Untitled";
+        //remote.getCurrentWindow().setTitle("Notepad - Untitled *");
+        isSaved=true;
+        break;
+    case 'open': //打开文件
+        askSaveIfNeed();
+        const files = remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+            filters: [
+                { name: "Text Files", extensions: ['txt', 'js', 'html', 'md'] },
+                { name: 'All Files', extensions: ['*'] } ],
+            properties: ['openFile']
+        });
+        if(files){
+            currentFile=files[0];
+            const txtRead=readText(currentFile);
+            txtEditor.value=txtRead;
+            document.title = "Notepad - " + currentFile;
+            isSaved=true;
+        }
+        break;
+    case 'save': //保存文件
+        saveCurrentDoc();
+        break;
+    case 'exiting':
+        askSaveIfNeed();
+        ipcRenderer.sendSync('reqaction', 'exit');
+        break;
+    }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+//读取文本文件
+function readText(file){
+    const fs = require('fs');
+    return fs.readFileSync(file, 'utf8');
+}
+//保存文本内容到文件
+function saveText(text, file){
+    const fs = require('fs');
+    fs.writeFileSync(file, text);
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+//保存当前文档
+function saveCurrentDoc(){
+    if(!currentFile){
+        const file = remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
+            filters: [
+                { name: "Text Files", extensions: ['txt', 'js', 'html', 'md'] },
+                { name: 'All Files', extensions: ['*'] } ]
+        });
+        if(file) currentFile=file;
+    }
+    if(currentFile){
+        const txtSave=txtEditor.value;
+        saveText(txtSave, currentFile);
+        isSaved=true;
+        document.title = "Notepad - " + currentFile;
+    }
+}
+
+//如果需要保存，弹出保存对话框询问用户是否保存当前文档
+function askSaveIfNeed(){
+    if(isSaved) return;
+    const response=dialog.showMessageBox(remote.getCurrentWindow(), {
+        message: 'Do you want to save the current document?',
+        type: 'question',
+        buttons: [ 'Yes', 'No' ]
+    });
+    if(response==0) saveCurrentDoc(); //点击Yes按钮后保存当前文档
+}
